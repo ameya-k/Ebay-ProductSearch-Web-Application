4528c4aa8fe494ef5d841796306d18c43b63817c
ameyakulkarni <abkulkar@usc.edu>
2019-03-30 16:13:33 -0700

    api key fixed

--- src/app/google-photos.service.ts
+++ src/app/google-photos.service.ts
@@ -9,7 +9,7 @@ export class GooglePhotosService {
   constructor(private googleHttp:HttpClient) { }
 
   getGooglePhotos(title):any{
-    let url="https://www.googleapis.com/customsearch/v1?q="+encodeURIComponent(title)+"&cx=016865113679854894358:-ggfxfoc38i&imgSize=huge&imgType=news&num=8&searchType=image&key=AIzaSyBZtSgUprgt5sQE9Mb3j7nnIOv-lpK43SM";
+    let url="https://www.googleapis.com/customsearch/v1?q="+encodeURIComponent(title)+"&cx=006048850498458678039:ugdiyxri87g&imgSize=huge&imgType=news&num=8&searchType=image&key=AIzaSyArcTzsunAfUsIr9pF0ZCpk-p7YMDI5-74";
     return this.googleHttp.get(url);
   }
 
